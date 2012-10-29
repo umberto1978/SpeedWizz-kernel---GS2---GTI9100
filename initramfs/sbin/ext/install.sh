004fdcb83e2c5be2dc8f91bb3cdbb8e16364b6e6
umberto1978 <ur.tecnotop@gmail.com>
2012-10-29 15:42:04 +0100

    Fixed an issue with preinstall script

--- initramfs/sbin/ext/install.sh
+++ initramfs/sbin/ext/install.sh
@@ -1,107 +1,121 @@
 #!/sbin/busybox sh
 
+BB="/sbin/busybox";
+
 extract_payload()
 {
-  payload_extracted=1
-  chmod 755 /sbin/read_boot_headers
-  eval $(/sbin/read_boot_headers /dev/block/mmcblk0p5)
-  load_offset=$boot_offset
-  load_len=$boot_len
-  cd /
-  dd bs=512 if=/dev/block/mmcblk0p5 skip=$load_offset count=$load_len | tar x
+	payload_extracted=1;
+  	$BB chmod 755 /sbin/read_boot_headers;
+  	eval $(/sbin/read_boot_headers /dev/block/mmcblk0p5);
+  	load_offset=$boot_offset;
+  	load_len=$boot_len;
+  	cd /;
+  	dd bs=512 if=/dev/block/mmcblk0p5 skip=$load_offset count=$load_len | tar x;
 }
 
-#. /res/customconfig/customconfig-helper
-#read_defaults
-#read_config
+. /res/customconfig/customconfig-helper;
+read_defaults;
+read_config;
 
-mount -o remount,rw /system
-/sbin/busybox mount -t rootfs -o remount,rw rootfs
-payload_extracted=0
+$BB mount -o remount,rw /system;
+$BB mount -t rootfs -o remount,rw rootfs;
+payload_extracted=0;
 
-cd /
+cd /;
 
-#if [ "$install_root" == "on" ];
-#then
-  if [ -s /system/xbin/su ];
-  then
-    echo "Superuser already exists"
-  else
-    if [ "$payload_extracted" == "0" ];then
-      extract_payload
-    fi
-    rm -f /system/bin/su
-    rm -f /system/xbin/su
-    mkdir /system/xbin
-    chmod 755 /system/xbin
-    xzcat /res/misc/payload/su.xz > /system/xbin/su
-    chown 0.0 /system/xbin/su
-    chmod 6755 /system/xbin/su
+#if [ "$install_root" == "on" ]; then
+	if [ -s /system/xbin/su ]; then
+		echo "Superuser already exists";
+	else
+		if [ "$payload_extracted" == "0" ]; then
+			extract_payload;
+		fi;
 
-    rm -f /system/app/*uper?ser.apk
-    rm -f /system/app/?uper?u.apk
-    rm -f /system/app/*chainfire?supersu*.apk
-    rm -f /data/app/*uper?ser.apk
-    rm -f /data/app/?uper?u.apk
-    rm -f /data/app/*chainfire?supersu*.apk
-    rm -rf /data/dalvik-cache/*uper?ser.apk*
-    rm -rf /data/dalvik-cache/*chainfire?supersu*.apk*
-    xzcat /res/misc/payload/Superuser.apk.xz > /system/app/Superuser.apk
-    chown 0.0 /system/app/Superuser.apk
-    chmod 644 /system/app/Superuser.apk
-  fi
+		# clean su traces
+		$BB rm -f /system/bin/su > /dev/null 2>&1;
+		$BB rm -f /system/xbin/su > /dev/null 2>&1;
+		$BB mkdir /system/xbin > /dev/null 2>&1;
+		$BB chmod 755 /system/xbin;
+
+		# extract SU binary
+		$BB xzcat /res/misc/payload/su.xz > /system/xbin/su;
+		$BB chown 0.0 /system/xbin/su;
+		$BB chmod 6755 /system/xbin/su;
+
+		# clean super user old apps
+		$BB rm -f /system/app/*uper?ser.apk > /dev/null 2>&1;
+		$BB rm -f /system/app/?uper?u.apk > /dev/null 2>&1;
+		$BB rm -f /system/app/*chainfire?supersu*.apk > /dev/null 2>&1;
+		$BB rm -f /data/app/*uper?ser.apk > /dev/null 2>&1;
+		$BB rm -f /data/app/?uper?u.apk > /dev/null 2>&1;
+		$BB rm -f /data/app/*chainfire?supersu*.apk > /dev/null 2>&1;
+		$BB rm -rf /data/dalvik-cache/*uper?ser.apk* > /dev/null 2>&1;
+		$BB rm -rf /data/dalvik-cache/*chainfire?supersu*.apk* > /dev/null 2>&1;
+
+		# extract super user app
+		$BB xzcat /res/misc/payload/Superuser.apk.xz > /system/app/Superuser.apk;
+		$BB chown 0.0 /system/app/Superuser.apk;
+		$BB chmod 644 /system/app/Superuser.apk;
+
+		# restore witch if exist
+		if [ -e /system/xbin/waswhich-bkp ]; then
+			$BB rm -f /system/xbin/which > /dev/null 2>&1;
+			$BB cp /system/xbin/waswhich-bkp /system/xbin/which > /dev/null 2>&1;
+			$BB chmod 755 /system/xbin/which > /dev/null 2>&1;
+		fi;
+
+		if [ -e /system/xbin/boxman ]; then
+			$BB rm -f /system/xbin/busybox > /dev/null 2>&1;
+			$BB mv /system/xbin/boxman /system/xbin/busybox > /dev/null 2>&1;
+			$BB chmod 755 /system/xbin/busybox > /dev/null 2>&1;
+			$BB mv /system/bin/boxman /system/bin/busybox > /dev/null 2>&1;
+			$BB chmod 755 /system/bin/busybox > /dev/null 2>&1;
+		fi;
+
+		# delete payload and kill superuser pid
+		$BB rm -rf /res/misc/payload;
+		pkill -f "com.noshufou.android.su" > /dev/null 2>&1;
+	fi;
+#fi;
+
+
+# liblights install by force to allow BLN
+if [ ! -e /system/lib/hw/lights.exynos4.so.BAK ]; then
+	$BB mv /system/lib/hw/lights.exynos4.so /system/lib/hw/lights.exynos4.so.BAK;
+fi;
+echo "Copying liblights";
+$BB cp -a /res/misc/lights.exynos4.so /system/lib/hw/lights.exynos4.so;
+$BB chown root:root /system/lib/hw/lights.exynos4.so;
+$BB chmod 644 /system/lib/hw/lights.exynos4.so;
+
+if [ ! -s /system/xbin/ntfs-3g ]; then
+	if [ "$payload_extracted" == "0" ]; then
+		extract_payload;
+  	fi;
+		$BB xzcat /res/misc/payload/ntfs-3g.xz > /system/xbin/ntfs-3g;
+		$BB chown 0.0 /system/xbin/ntfs-3g;
+		$BB chmod 755 /system/xbin/ntfs-3g;
 fi;
 
 echo "Checking if cwmanager is installed"
 if [ ! -f /system/.siyah/cwmmanager3-installed ];
 then
-  if [ "$payload_extracted" == "0" ];then
-    extract_payload
-  fi
-  rm /system/app/CWMManager.apk
-  rm /data/dalvik-cache/*CWMManager.apk*
-  rm /data/app/eu.chainfire.cfroot.cwmmanager*.apk
+  if [ "$payload_extracted" == "0" ]; then
+    extract_payload;
+  fi;
+  $BB rm /system/app/CWMManager.apk;
+  $BB rm /data/dalvik-cache/*CWMManager.apk*;
+  $BB rm /data/app/eu.chainfire.cfroot.cwmmanager*.apk;
 
-  xzcat /res/misc/payload/CWMManager.apk.xz > /system/app/CWMManager.apk
-  chown 0.0 /system/app/CWMManager.apk
-  chmod 644 /system/app/CWMManager.apk
-  mkdir /system/.siyah
-  chmod 755 /system/.siyah
+  $BB xzcat /res/misc/payload/CWMManager.apk.xz > /system/app/CWMManager.apk;
+  $BB chown 0.0 /system/app/CWMManager.apk;
+  $BB chmod 644 /system/app/CWMManager.apk;
+  $BB mkdir /system/.siyah;
+  $BB chmod 755 /system/.siyah;
   echo 1 > /system/.siyah/cwmmanager3-installed
-fi
+fi;
 
-echo "liblights..."
-romtype=`cat /proc/sys/kernel/rom_feature_set`
-# only for non-cm7 roms
-#if [ "${romtype}a" == "0a" ];
-#then
-#if [ ! -f /system/.siyah/liblights-installed ];then
-  lightsmd5sum=`/sbin/busybox md5sum /system/lib/hw/lights.exynos4.so | /sbin/busybox awk '{print $1}'`
-  blnlightsmd5sum=`/sbin/busybox md5sum /res/misc/lights.exynos4.so | /sbin/busybox awk '{print $1}'`
-  if [ "${lightsmd5sum}a" != "${blnlightsmd5sum}a" ];
-  then
-    echo "Copying liblights"
-    /sbin/busybox mv /system/lib/hw/lights.exynos4.so /system/lib/hw/lights.exynos4.so.BAK
-    /sbin/busybox cp /res/misc/lights.exynos4.so /system/lib/hw/lights.exynos4.so
-    /sbin/busybox chown 0.0 /system/lib/hw/lights.exynos4.so
-    /sbin/busybox chmod 644 /system/lib/hw/lights.exynos4.so
-  fi
-  echo 1 > /system/.siyah/liblights-installed
-#fi
-#fi
+$BB rm -rf /res/misc/payload;
 
-echo "ntfs-3g..."
-if [ ! -s /system/xbin/ntfs-3g ];
-then
-  if [ "$payload_extracted" == "0" ];then
-    extract_payload
-  fi
-  xzcat /res/misc/payload/ntfs-3g.xz > /system/xbin/ntfs-3g
-  chown 0.0 /system/xbin/ntfs-3g
-  chmod 755 /system/xbin/ntfs-3g
-fi
-
-rm -rf /res/misc/payload
-
-/sbin/busybox mount -t rootfs -o remount,ro rootfs
-mount -o remount,ro /system
+$BB mount -t rootfs -o remount,rw rootfs;
+$BB mount -o remount,rw /system;
